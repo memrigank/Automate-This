5c9ea7403cea0f84d9b5742336259fcf9e8ca26a
memrigank <er.mrigank02@gmail.com>
2025-01-21 22:55:51 +0530

    added Override annotation

--- PolymorphismForAutomateThis/src/creditCardPayment.java
+++ PolymorphismForAutomateThis/src/creditCardPayment.java
@@ -4,10 +4,12 @@ public class creditCardPayment extends basePayments{
         super(creditCard, CVV);
     }
 
+    @Override
     public void billPayment(){
         System.out.println("bill Payment done using Credit Card");
     }
 
+    @Override
     public void addCardsToFlipkart(){
         System.out.println("Credit Card has been added with number: " + getCreditCard());
     }
--- PolymorphismForAutomateThis/src/debitCardPayment.java
+++ PolymorphismForAutomateThis/src/debitCardPayment.java
@@ -4,10 +4,12 @@ public class debitCardPayment extends basePayments{
         super(debitCard, mobileNumber);
     }
 
+    @Override
     public void billPayment(){
         System.out.println("bill Payment done using Debit Card");
     }
 
+    @Override
     public void addCardsToFlipkart(){
         System.out.println("Debit Card has been added with number: " + getDebitCard() + " Registered Mobile Number is "+ getMobileNumber());
     }
