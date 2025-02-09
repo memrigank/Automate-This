1a0030a780f5d5b9b5a95dd9d339a11c6d389851
memrigank <er.mrigank02@gmail.com>
2025-02-09 09:34:23 +0530

    Added Singleton Class

---
+++ AutomatingMyntra/src/main/java/baseClass.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class baseClass {
+
+    private static WebDriver driver;
+
+    private baseClass(){};
+
+    public static WebDriver getInstance(){
+        if(driver==null){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+}
